978138b7328c8f8bb9e21475c2b49f9447bb8b13
lupaslee <pablo@fernnadezmiquel.com>
2018-01-02 21:40:08 +0100

    Statitcs mesasges added.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,8 +9,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int scoreA = 0;
     int scoreB = 0;
-    int tripleA = 0;
-    int tripleB = 0;
+    int statTripleA = 0;
+    int statTripleB = 0;
+    int statDobleA = 0;
+    int statDobleB = 0;
+    int statFreeThrowA = 0;
+    int statFreeThrowB = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +23,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         refreshDisplayScore();
-        refreshStats();
+        refreshStats(0);
     }
 
-    public void refreshStats() {
+    public void refreshStats(int point) {
+
+
         Resources res = getResources();
-        String recurso = res.getString(R.string.stat_display);
-        String stringFormateada = String.format(recurso, tripleA, tripleB);
+        if (point == 3) {
+            String statString = res.getString(R.string.stat_display_triples);
+            String textStatics = String.format(statString, statTripleA, statTripleB);
+            TextView text = findViewById(R.id.statics_dispay);
+            text.setText(textStatics);
+        } else if (point == 2) {
+            String statString = res.getString(R.string.stat_display_2_point);
+            String textStatics = String.format(statString, statDobleA, statDobleB);
+            TextView text = findViewById(R.id.statics_dispay);
+            text.setText(textStatics);
+        } else if (point == 1) {
+            String statString = res.getString(R.string.stat_display_freethrow);
+            String textStatics = String.format(statString, statFreeThrowA, statFreeThrowB);
+            TextView text = findViewById(R.id.statics_dispay);
+            text.setText(textStatics);
+        } else if (point == 0) {
+            String statString = res.getString(R.string.stat_display_play);
+            String textStatics = String.format(statString);
+            TextView text = findViewById(R.id.statics_dispay);
+            text.setText(textStatics);
 
-        TextView text = findViewById(R.id.statics_dispay);
-        text.setText(stringFormateada);
+        }
+
     }
+
+
+
 
     /**
      * Refresh display of both teams.
@@ -47,45 +75,69 @@ public class MainActivity extends AppCompatActivity {
     public void pointTreeA(View view) {
 
         scoreA = scoreA + 3;
-        tripleA = tripleA + 1;
+        statTripleA = statTripleA + 1;
         refreshDisplayScore();
-        refreshStats();
+        refreshStats(3);
     }
 
     /*Add 2 point team A*/
     public void pointTwoA(View view) {
 
         scoreA = scoreA + 2;
+        statDobleA = statDobleA + 1;
         refreshDisplayScore();
+        refreshStats(2);
     }
 
     public void pointFreeA(View view) {
 
         scoreA = scoreA + 1;
+        statFreeThrowA = statFreeThrowA + 1;
         refreshDisplayScore();
+        refreshStats(1);
     }
 
     /*Add 3 point team B*/
     public void pointTreeB(View view) {
 
         scoreB = scoreB + 3;
-        tripleB = tripleB + 1;
+        statTripleB = statTripleB + 1;
         refreshDisplayScore();
-        refreshStats();
+        refreshStats(3);
+
     }
 
     /*Add 2 point team B*/
     public void pointTwoB(View view) {
 
         scoreB = scoreB + 2;
+        statDobleB = statDobleB + 1;
         refreshDisplayScore();
+        refreshStats(2);
     }
 
     public void pointFreeB(View view) {
 
         scoreB = scoreB + 1;
+        statFreeThrowB = statFreeThrowB + 1;
         refreshDisplayScore();
+        refreshStats(1);
     }
+
+    public void resetAll(View view) {
+
+        scoreA = 0;
+        scoreB = 0;
+        statTripleA = 0;
+        statTripleB = 0;
+        statDobleA = 0;
+        statDobleB = 0;
+        statFreeThrowA = 0;
+        statFreeThrowB = 0;
+        refreshDisplayScore();
+        refreshStats(0);
+    }
+
 
 
 }
